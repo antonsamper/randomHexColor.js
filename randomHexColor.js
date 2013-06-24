8b38a47a2090f223e8444a5308cc09ad631a5d08
Anton Samper Rivaya <anton@aeser.co.uk>
2013-06-24 08:32:51 +0100

    reduced code

--- randomHexColor.js
+++ randomHexColor.js
@@ -2,17 +2,10 @@ var randomHexColor = (function () {
 
     'use strict';
 
-    var values          = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'],
-        valuesLength    = values.length;
-
     return function () {
 
-        var color   = '#',
-            length  = 7;
+        return '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
 
-        while (--length) color += values[~~(Math.random() * valuesLength)];
-
-        return color;
     };
 
 }());
